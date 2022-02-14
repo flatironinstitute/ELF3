77512642afecf113f0b31f1bcbce636e337fce17
rlindsa2 <99688178+rlindsa2@users.noreply.github.com>
2022-02-14 16:56:52 -0500

    Update polyq_dist.py

--- polyq_dist.py
+++ polyq_dist.py
@@ -13,7 +13,12 @@ n_bins=50
 
 ##How to make the bars fill their bins? (not be skinny)
 plt.figure(figsize=(8.5,6.25), dpi=300)
-plt.hist([pq290,pq300,pq320,pq415],bins=n_bins,label=['290K','300K','320K','415K'],color=['black','skyblue','orange','green'],rwidth=1)
+
+plt.hist([pq290],bins=n_bins,label=['290K'],edgecolor='black')
+plt.hist([pq300],bins=n_bins,label=['300K'],edgecolor='black')
+plt.hist([pq320],bins=n_bins,label=['320K'],edgecolor='black')
+plt.hist([pq415],bins=n_bins,label=['415K'],edgecolor='black')
+
 plt.legend(loc='upper left',fontsize=20)
 plt.xlabel('C-alpha Distance (nm)', fontsize=20)
 plt.ylabel('# Structures', fontsize=20)
